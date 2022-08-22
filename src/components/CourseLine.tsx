2f1261764ab7bb58b4552aaac17f84a5689c9757
yurzhang <yurzhang@163.com>
2022-08-22 22:58:42 +0800

    feat: add confirm step when drop courses

--- src/components/CourseLine.tsx
+++ src/components/CourseLine.tsx
@@ -105,12 +105,39 @@ export const CourseLine = (props: ICourseLineProps) => {
         )}
       </Th>
       <Th>
-        <Button
-          size="xs"
-          onClick={() => props.dropCourse(props.course.id)}
-        >
-          退课
-        </Button>
+        <Popover>
+          <PopoverTrigger
+            as={Button}
+            size="xs"
+            variant="subtle"
+          >
+            退课
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverHeader>
+              <div>
+                <span>您真的要退课吗？</span>
+                {props.course.pinned && (
+                  <>
+                    <br />
+                    <span style={{ color: "red" }}>
+                      注意：该课程当前为已选中状态！
+                    </span>
+                  </>
+                )}
+              </div>
+            </PopoverHeader>
+            <PopoverBody>
+              <Button
+                size="xs"
+                onClick={() => props.dropCourse(props.course.id)}
+              >
+                确定
+              </Button>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </Th>
     </Tr>
   );
